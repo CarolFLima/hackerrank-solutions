5c8bedf34897f315244a6fb69db9685a54c8a917
Caroline Lima <carolflima0@gmail.com>
2021-05-20 07:52:43 -0300

    fix TLE for test case 9

--- problem-solving/stack/texteditor.py
+++ problem-solving/stack/texteditor.py
@@ -1,25 +1,29 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
+from sys import stdout
+
 Q = int(input())
 
 S = ''
 history = []
-
+l = []
 
 for _ in range(Q):
-    query = input().strip().split()
-    operation = query[0]
-    
-    if operation == '1':
+    query = input().strip().split(' ')
+
+    if query[0] == '1':
         history.append(S)
         S = S + query[1]
-    elif operation == '2':
+    elif query[0] == '2':
         history.append(S)
-        k = int(query[1])        
-        S = S[0 : len(S) - k ]
-    elif operation == '3':
-        k = int(query[1])
-        print(S[k-1: k ])
+        k = int(query[1])   
+        bound = len(S) - k
+        S = S[0 : bound ]
+    elif query[0] == '3':
+        k = int(query[1]) - 1
+        l.append(S[k])
     else:
         S = history.pop()
 
+
+stdout.write('\n'.join(l))
